4b578e925cbe1274226819c8ea454a3c0aed9bae
sparkjames <james@sparkexperience.com>
2016-05-16 20:02:41 -0400

    javascript stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,47 @@
 //console.log(document);
 //document.querySelectorAll('.my #awesome selector');
 
+// NAVIGATION
 menu_button = document.getElementById('nav__toggle');
+console.log(menu_button);
 nav_element = document.getElementById('nav__main');
+console.log(nav_element);
 
 menu_button.addEventListener('click', toggle_nav);
 function toggle_nav(){
-    className = 'active';
-    
-    if (nav_element.classList) {
-        nav_element.classList.toggle(className);
-    } else {
-        var classes = nav_element.className.split(' ');
-        var existingIndex = classes.indexOf(className);
+    //toggleActive(nav_element);
+    toggleActive(document.body);
 
-        if (existingIndex >= 0)
-        classes.splice(existingIndex, 1);
-        else
-        classes.push(className);
-
-        nav_element.className = classes.join(' ');
-    }
+    // Get the difference in width between the navigation and the menu button.
+    width_diff = nav_element.offsetWidth - menu_button.offsetWidth;
+    var hamburger = document.querySelectorAll('#nav__toggle .icon-hamburger');
+    console.log(hamburger);
+    // Use that difference as the margin-left for the hamburger icon. This pushes the word 'Menu' to the left without moving the hamburger. Adding the width difference should make the nav and the button appear to be at equal widths. Using calc() for an additional .5em to account for the difference in padding between the menu items and the button.
+    hamburger[0].style.marginLeft = 'calc('+width_diff+'px + .5em)';
 }
 
 
 
 
+
+
+// FORM STUFF
+pw_toggle = document.getElementById('signup__password--toggle');
+
+pw_toggle.addEventListener('click', toggle_pw_field);
+function toggle_pw_field(e){
+    e.preventDefault();
+
+    var pw_field = document.getElementById('signup__password');
+    if( pw_field.type=='password' ){
+        pw_field.type = 'text';
+
+    } else {
+        pw_field.type = 'password';
+    }
+
+    return false;
+}
 
 
 
@@ -109,5 +125,23 @@ function removeClasses(element){
         element.classList.remove('has-error');
     } else {
         element.className = '';
+    }
+}
+
+function toggleActive(element){
+    className = 'active-nav';
+
+    if (element.classList) {
+        element.classList.toggle(className);
+    } else {
+        var classes = element.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push(className);
+
+        element.className = classes.join(' ');
     }
 }
